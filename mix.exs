206ad2d42e09afb2c16396d890c67ae0c4d2e5f0
alex <contact@alex-min.fr>
2023-10-28 11:14:20 +0200

    new version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LiveViewNativeFlutter.MixProject do
   def project do
     [
       app: :live_view_native_flutter,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.15",
       description: "LiveView Native platform for Flutter",
       package: package(),
